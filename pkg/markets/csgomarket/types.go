165089ddf65e8e8a3be23c00dc6e3e825f745d32
arseniizyk <arseniizyk@gmail.com>
2025-07-12 18:23:37 +0300

    CSGOMarket: optimization

--- pkg/markets/csgomarket/types.go
+++ pkg/markets/csgomarket/types.go
@@ -14,15 +14,10 @@ type csgoMarket struct {
 }
 
 type Response struct {
-	Success  bool   `json:"success"`
-	Error    string `json:"error"`
-	Currency string `json:"currency"`
-	Data     []struct {
-		MarketHashName string `json:"market_hash_name"`
-		Price          int    `json:"price"`
-		Class          int    `json:"class"`
-		Instance       int    `json:"instance"`
-		Count          int    `json:"count"`
+	Success bool `json:"success"`
+	Data    []struct {
+		Price int `json:"price"`
+		Count int `json:"count"`
 	} `json:"data"`
 }
 
